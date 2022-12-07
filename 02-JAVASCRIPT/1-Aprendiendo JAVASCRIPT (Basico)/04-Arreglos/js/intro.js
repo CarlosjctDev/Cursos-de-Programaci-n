854c60d89156826f6339e7543e783836033d957f
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2022-12-07 11:15:30 -0500

    Se agrega proyecto Arreglos

---
+++ 02-JAVASCRIPT/1-Aprendiendo JAVASCRIPT (Basico)/04-Arreglos/js/intro.js
@@ -0,0 +1,10 @@
+//Arreglos
+var amigos=["Carlos","Nicolle","Laura","Jose"]
+
+//Para acceder seria
+//<br> sirve para hacer saltos de lineas
+document.write("1-Nombre de nuestro arreglo: "+amigos[0]+"<br>");
+
+//Para cambiarle el valor
+amigos[0]=prompt("Ingrese su nombre")
+document.write("2-Nombre Actualizado: "+amigos[0]);
