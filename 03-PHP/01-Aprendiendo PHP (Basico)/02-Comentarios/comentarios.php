9753a978e84049bbd5a3db357051eafadeed6135
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2022-12-30 21:23:34 -0500

    Se agrega proyecto comentarios

---
+++ 03-PHP/01-Aprendiendo PHP (Basico)/02-Comentarios/comentarios.php
@@ -0,0 +1,13 @@
+<!-- Los comentarios no se ven en la pantalla, sirven para indicar que hace cada linea de codigo
+    o tambien puede servir para colocar quien desarrollo el codigo -->
+<?php
+
+// Comentario de una sola linea
+echo "Hola Mundo1";
+
+## Comentario de una sola linea
+echo " Hola Mundo2";
+
+/* Comentario de multiples
+    lineas*/
+echo " Hola Mundo3";    
