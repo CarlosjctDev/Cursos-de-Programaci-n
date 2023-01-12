9934edff1b2a7cd76127f2e887239c9ad63949cc
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-01-12 11:18:02 -0500

    Se agrega proyecto Concatenacion e interpolacion

---
+++ 03-PHP/01-Aprendiendo PHP (Basico-Avazando)/07-Concatenacion e interpolacion/concatenacion.php
@@ -0,0 +1,11 @@
+<!-- Concatenacion -->
+
+<?php
+$Nombre="Carlos ";
+$Pais="Colombia ";
+$Celular="30043*****";
+
+// El punto (.) se utiliza para concatenar string o variables
+$Resultado="Mi nombre es: ". $Nombre."<br> Pais: ".$Pais."<br> Celular:".$Celular;
+
+echo $Resultado;
---
+++ 03-PHP/01-Aprendiendo PHP (Basico-Avazando)/07-Concatenacion e interpolacion/interpolacion.php
@@ -0,0 +1,10 @@
+<!-- Interpolacion, se usa sin necesidad de puntos, solo entre dos comillas dobles una que abre y una que cierra -->
+
+<?php
+$Nombre="Carlos ";
+$Pais="Colombia ";
+$Celular="30043*****";
+
+$Resultado="Mi nombre es: {$Nombre} <br> Pais: $Pais<br> Celular:$Celular";
+
+echo $Resultado;
