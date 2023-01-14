9bb1ef1300df39f8f03bca72319cdc7720e76866
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-01-14 11:53:17 -0500

    Se agrega proyecto Operador ternario

---
+++ 03-PHP/01-Aprendiendo PHP (Basico-Avazando)/18-Operador ternario/operador_ternario.php
@@ -0,0 +1,13 @@
+<!-- Operador ternario
+    Puede considerarse como una instrucción if de una sola linea. Se compone de tres partes.
+    El operator, dos resultados. La sintaxis es la siguiente:
+    
+    <operator>?<true value>;<false value>
+        
+-->
+
+<?php
+
+#ejemplo
+$total=(9<7)? 10*7: 10*5;
+echo $total;
