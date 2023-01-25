aee6365b993902290c2fb1d8b11bebf5f495b5e9
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-01-25 16:46:51 -0500

    Se agrega proyecto incluir funciones desde otro archivo (Como mover entre carpetas)

---
+++ 03-PHP/01-Aprendiendo PHP (Basico-Avazando)/31-incluir funciones desde otro archivo (Como mover entre carpetas)/funciones/funciones.php
@@ -0,0 +1,7 @@
+<?php
+
+function promedio_alumno ($nota_1,$nota_2,$nota_3){
+    echo "<br>";
+    $promedio=($nota_1+$nota_2+$nota_3)/3;
+    return $promedio;
+}
---
+++ 03-PHP/01-Aprendiendo PHP (Basico-Avazando)/31-incluir funciones desde otro archivo (Como mover entre carpetas)/incluir_funciones.php
@@ -0,0 +1,8 @@
+<?php
+
+#Se puede colocar include o require
+##para regresar atras una carpeta se utiliza (../)
+##para ingresar a una carpeta seria:
+include "funciones/funciones.php";
+
+echo "El promedio es: ".promedio_alumno(7,3,9);
