d56bd1aa7f669ff4993bfe0d6e612a52bedcac61
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-02-02 18:40:44 -0500

    Se agrega proyecto diferencias entre cookies y sesiones

---
+++ 03-PHP/01-Aprendiendo PHP (Basico-Avazando)/47-diferencias entre cookies y sesiones/cookies_sesion.php
@@ -0,0 +1,10 @@
+<!--                           Diferencia entre cookies y sesion
+            Cookies                                             Sesiones
+    Se almacena la informacion en local,                Se almacenan la informacion en el servidor.
+    es decir en el navegador.                           Sirve para crear login.
+    No se almacenan informacion sensible.               
+    Se utiliza para el idioma.                           
+
+    Se pueden editar o borrar.                          Es mas seguro que las cookies.
+
+-->
