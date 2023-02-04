07f9268f8eaa276cbaec0411d2cebafe27da90fd
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-02-04 11:27:13 -0500

    Se agrega carpeta Mysql y proyecto introduccion Mysql

---
+++ 04-Mysql/01-Aprendiendo Mysql (Basico-Avanzado)/01-Introduccion Mysql/Introducion.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+/* Introducion a Mysql
+    Crearemos un sistema de inventarios web con php y Mysql
+    Login
+    CRUD
+    Motrar productos por categorias
+    Actualizar datos
+    etc
+    Utilizaremos framework bulma
+    */
