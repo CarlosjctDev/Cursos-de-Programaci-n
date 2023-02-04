7fecaba014e7a3066352451c9033c9b4924aafc1
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-02-04 11:36:22 -0500

    Se agrega proyecto Crear una base de datos en Mysql con phpmyadmin

---
+++ 04-Mysql/01-Aprendiendo Mysql (Basico-Avanzado)/02-Crear una base de datos en Mysql con phpmyadmin/base_datos.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Se ingresa a phpmyadmin si es local se ingresa
+    con la cuenta root y la contraseña va vacia
+    Para crear una base de datos le damos en nueva
+    Le colocamos un nombre 
+    Se va llamar inventario
+    Elegimos un cotejamiento en este caso utilizaremos utf8-spanish2_ci 
+    y le damos crear. */
