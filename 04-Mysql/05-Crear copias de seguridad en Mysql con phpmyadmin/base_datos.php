e648d9378a148511b7e36cbac12ac2125961db9c
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-02-07 10:37:34 -0500

    Se agrega proyecto Crear copias de seguridad en Mysql con phpmyadmin

---
+++ 04-Mysql/05-Crear copias de seguridad en Mysql con phpmyadmin/base_datos.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+    Crear copias de seguridad(BackUp)
+    Le damos a la base de datos
+    Le damos exportar
+    Podemos seleccionar el metodo rapido
+    o el metodo personalizado
+    la diferencia es que con el metodo rapido tocaria crear la base de dato
+    y el personalizado si le damos en AGREGAR SENTENCIA CREATE DATABASE/USE
+    se crearia completamente.
+
+
+    
+    */
