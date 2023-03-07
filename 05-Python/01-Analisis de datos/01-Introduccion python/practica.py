2bfc1e98db4609c18df03a5748feb7ba306da8e6
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-07 01:03:33 -0500

    Se agrega carpeta Python y Analisis de datos y proyecto Introduccion python

---
+++ 05-Python/01-Analisis de datos/01-Introduccion python/practica.py
@@ -0,0 +1,11 @@
+#Introduccion
+# Comandos Magicos
+#%pwd : devuelve el directorio en el cual nos encontramos
+#%cd .. : sirve para regresar una carpeta
+""" %ls : mostrar carpetas o directorios , acepta diferentes banderas
+    %ls -l """
+# %bookmark 01-Analisis de datos , guarda el directorio
+# %cd -b 01-Analisis de datos , regresa al directorio
+# %store sirve para almacenar variables
+# %store -r  restablece las variables almacenadas
+"""%lsmagic   lista todos los comandos magicos que existen."""
