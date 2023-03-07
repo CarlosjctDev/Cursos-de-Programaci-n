68aab80958830845fab7624be9df38e29a3a5906
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-07 10:57:01 -0500

    Se agrega proyecto Crear archivos con ipython

---
+++ 05-Python/01-Analisis de datos/04-Crear archivos con ipython/main.py
@@ -0,0 +1,4 @@
+# Crear archivos con ipython
+#%save operaciones.py 11-12   permite crear un archivo y darle los codigos que se crearon en ipython
+#                             el 11-12 representan las lineas que queremos guardar del 11 hasta 12 
+#%save -a operaciones.py 13   Si le deseamos agregar mas contenido mediante ipython
---
+++ 05-Python/01-Analisis de datos/04-Crear archivos con ipython/operaciones.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+def resta(a: int , b: int) -> int:
+    """"Permite restar dos numeros enteros"""
+    result= a - b
+    return result
+print(resta(20,10))
+def suma(a: int , b: int) -> int:
+    """"Permite sumar dos numeros enteros"""
+    result= a + b
+    return result
+print(suma(20,10))
