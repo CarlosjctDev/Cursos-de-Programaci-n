e379d195d98a6dbe39d49144cfb1c045ae7b57d8
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-07 13:21:06 -0500

    Se agrega proyecto (numpy-arreglos)

---
+++ 05-Python/01-Analisis de datos/09-Obtener y actualizar elementos (numpy-arreglos)/main.py
@@ -0,0 +1,12 @@
+# Numpy  pip install numpy
+# Obtener y actualizar elementos dentro del arreglo
+"""
+    import numpy as np                          estandar
+    a=np.random.randint(0,10,10)                Crear arreglos de numeros enteros aleatorios, recibe 3 argumentos
+                                                    los primeros dos hacen referencia al rango, el tercer arg
+                                                    se refiere a la longitud que queremos que el arreglo posea.
+    a[2]                                        Se puede obtener los datos del array de izquierda a derecha
+                                                    empezando por 0
+    a[-1]                                       Se puede obtener los datos del array de derecha a izquierda,
+                                                    cuando se usan negativos, empezando por -1            
+"""
