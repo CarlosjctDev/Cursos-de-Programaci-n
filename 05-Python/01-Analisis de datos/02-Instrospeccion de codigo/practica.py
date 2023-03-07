3951b6226a69601211f4bfd3a53ee892c4937340
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-07 10:15:02 -0500

    Se agrega proyecto Introspeccion de codigo

---
+++ 05-Python/01-Analisis de datos/02-Instrospeccion de codigo/practica.py
@@ -0,0 +1,12 @@
+#Introduccion
+# Introspección del código
+"""Definir una funcion que sume dos numero enteros en ipython
+     def suma(a:int , b:int) -> int:
+        #Permite sumar dos numeros enteros.
+        return  a + b
+    Si queremos saber el codigo de la funcion: suma??
+    si quiero saber de que tipos de datos tiene la funcion: suma?
+        los signos de interrogacion no serviran mucho cuando no hayamos 
+        realizado nosotros el codigo
+        """
+
