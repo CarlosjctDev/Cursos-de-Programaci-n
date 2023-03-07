e9f2acde8c2ef5cb41a8c320c1c1148532b98d38
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-07 11:32:49 -0500

    Se agrega proyecto Operaciones (numpy)

---
+++ 05-Python/01-Analisis de datos/06-Operaciones (numpy)/main.py
@@ -0,0 +1,13 @@
+# Numpy  pip install numpy
+# Operaciones
+"""
+    import numpy as np                  estandar
+    np.array([1,2,3,4,5])               recibe como argumento un listado, returna un arreglo
+    a=np.array([1,2,3,4,5])             se le puede añadir una variable
+    a+10 a-10 a*10 a/10                 se puede sumar, restar, multiplicar y dividir
+    b=np.array([6,7,8,9,10]) 
+    a+b                                 se pueden realizar todas las operaciones siempre y cuando el arreglo
+                                            tenga el mismo valor de numeros.    
+
+
+"""
