ffb96ad7fad17ef01f67f674061b0482d6ba221a
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-07 11:41:15 -0500

    Se agrega proyecto Tipos de datos (numpy)

---
+++ 05-Python/01-Analisis de datos/07-Tipos de datos (numpy)/main.py
@@ -0,0 +1,13 @@
+# Numpy  pip install numpy
+# Tipos de datos
+"""
+    import numpy as np                          estandar
+    np.array([1,2,3,4,5])                       recibe como argumento un listado, returna un arreglo
+    a=np.array([1,2,3,4,5])                     se le puede añadir una variable
+    a.dtype                                     visualiza el tipo  de dato     
+    a=np.array([1,2,3,4,5], dtype=float)        cambia el tipo de dato, puede recibir de parametro float,
+                                                    str,bool (en python cualquier valor que sea diferente
+                                                    a 0 sera tomado como verdadero.)
+    a=np.array([1,2,3,4,5], dtype=np.complex)   Almacena numeros complejos
+
+"""
