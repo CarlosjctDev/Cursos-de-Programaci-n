a5e5e108d836e75e9775defb2c6f86c9b4e47958
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-07 11:23:12 -0500

    Se agrega proyecto Arreglos (numpy)

---
+++ 05-Python/01-Analisis de datos/05-Arreglos (numpy)/main.py
@@ -0,0 +1,16 @@
+# Numpy  pip install numpy
+# Arreglos
+"""
+    import numpy as np                  estandar
+    np.array([1,2,3,4,5])               recibe como argumento un listado, returna un arreglo
+    a=np.array([1,2,3,4,5])             se le puede añadir una variable
+    type(a)                             Se confirma el tipo
+    a.size                              Cantidad de elementos que almacena un arreglo
+    a.ndim                              cantidad de dimensiones que posee el arreglo
+    a.dtype                             Que tipos de datos almacena el arreglo
+    a.shape                             la cantidad de elementos y las dimensiones que posee el arreglo
+    Una vez que modificamos la longitud de un arreglo esta longitud no podra ser modificada a diferencia
+    de  una lista donde se puede añadir o eliminar elementos de la lista en los arreglos esto no es posible
+    una vez definimos la longitud de un arreglo este se quedara con dicha longitud por el resto del programa
+
+"""
