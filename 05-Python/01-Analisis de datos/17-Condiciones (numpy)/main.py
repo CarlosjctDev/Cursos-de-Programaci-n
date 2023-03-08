d4b629814a04eb522d9e85c3a6baeea1ebfadec9
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-08 06:25:12 -0500

    Se agrega proyecto Condiciones (numpy)

---
+++ 05-Python/01-Analisis de datos/17-Condiciones (numpy)/main.py
@@ -0,0 +1,10 @@
+# Numpy  pip install numpy
+# Condiciones
+"""
+    import numpy as np                      estandar
+    a=np.random.randint(0,100,50)       Arreglo de 50 numeros aleatorios de 0 a 100
+    np.all(a > 50)                      Si todos nuestros elementos son mayor que 50  devuelve true o false
+                                            dependiendo si cumple las condiciones
+    np.any(a > 10)                      Nos permite conocer si por lo menos un elemento cumple con las condiciones.
+                                            va returnar verdadero si un elemento cumple con la condicion
+    """
