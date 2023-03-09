ca18915bdcfdea92cca1c535f2c2ecfc2ccf19b6
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-09 12:01:43 -0500

    Se agrega proyecto Crear series con diccionarios (pandas)

---
+++ 05-Python/01-Analisis de datos/24-Crear series con diccionarios (pandas)/main.py
@@ -0,0 +1,15 @@
+# Pandas
+# pip install pandas
+# Crear Series
+"""Crear series utilizando diccionarios"""
+"""
+    import pandas as pd                         estandar
+    colores={                                   Crear un diccionario
+        'rojo':100,
+        'verde':200,
+        'azul':300,
+        'negro':400
+    }
+    a=pd.Series(colores)                        Crea la serie con un diccionario
+    
+    """
