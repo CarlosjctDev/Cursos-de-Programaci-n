18d9e4aa86112210fed0beccebca1ce075126375
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-09 11:04:38 -0500

    Se agrega proyecto Ordenar arreglos (numpy)

---
+++ 05-Python/01-Analisis de datos/22-Ordenar arreglos (numpy)/main.py
@@ -0,0 +1,12 @@
+# Numpy  pip install numpy
+# Ordenar arreglos
+"""
+    import numpy as np                          estandar
+    a=np.random.randint(0,10,20)                Arreglo de 10 numeros aleatorios de 0 a 20
+    a.sort()                                    Ordena el arreglo de forma ascendente (modifica el arreglo)
+    a=a[::-1]                                   Ordenar el arreglo de forma inversa
+    b=np.random.randint(0,10,20)                Arreglo de 10 numeros aleatorios de 0 a 20
+    np.sort(b)                                  Ordena el arreglo de forma ascendente (No modifica el arreglo)
+    c=np.sort(b)[::-1]                          Ordenar el arreglo de forma inversa 
+
+    """
