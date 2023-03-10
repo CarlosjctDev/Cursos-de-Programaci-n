2f1b0a1e1492f1c1fa024b096453d5be6986cd87
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-10 16:36:49 -0500

    Se agrega proyecto Limpieza de datos (pandas)

--- 05-Python/01-Analisis de datos/30-Limpieza de datos (pandas)/main.py
+++ 05-Python/01-Analisis de datos/30-Limpieza de datos (pandas)/main.py
@@ -9,6 +9,6 @@
     data= pd.read_csv('users.csv', index_col='id') Especificar a pandas la columna que se va tomar  como id
     data.dropna()                                   Elimina las filas que tengan NaN (crea un nuevo dataframe)
     data.fillna('Nuevo valor')                      Colocar valores en NaN (crea un nuevo dataframe)
-    la mejor forma de llenar valores ausentes seria con un diccionario
+    la mejor forma de llenar valores ausentes seria con un diccionario 
     data.fillna({'nombre': 'Sin nombre', 'email': 'example@example.com'}) De esta forma se llenan valores ausente
     """
