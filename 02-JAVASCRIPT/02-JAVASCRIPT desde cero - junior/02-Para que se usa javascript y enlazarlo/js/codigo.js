9b5d2df62062bd4614c657bde6b630cf1bf8e74a
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-27 12:01:58 -0500

    Se agrega proyecto Para que se usa javascript y enlazarlo

---
+++ 02-JAVASCRIPT/02-JAVASCRIPT desde cero - junior/02-Para que se usa javascript y enlazarlo/js/codigo.js
@@ -0,0 +1,22 @@
+/* 
+Para que se usa javascript? y enlazarlo
+Lo mas comun:
+-Dinamismo de los sitios web (dinamico del lado del cliente)
+para que sea dinamicos del lado del servidor se usa
+NodeJS
+Hay varios framework que reducen codigo, como Angular, React
+o Vue.JS
+
+Otros usos no tan comunes:
+-Inteligencia artificial, google creo una libreria que se llama
+    tensor flow
+-Placas electronicas  atraves de NodeJS se puede programar
+    con una tecnologia que se llama (Jhonny Five) placas
+    electronicas como arduino
+-Mobile Apps, se pueden crear aplicaciones mobiles mediante
+    uno o otro compilador un poco extraño
+-Desktop App , etc
+
+Se enlaza en el HTML 
+*/
+alert("Hola desde javascript")
