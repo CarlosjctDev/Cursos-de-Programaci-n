0c4004eeda0016410a05f5423edb0e54aac4f6b0
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-30 11:27:40 -0500

    Se agrega proyecto Camel case

---
+++ 02-JAVASCRIPT/02-JAVASCRIPT desde cero - junior/07-Camel case/js/codigo.js
@@ -0,0 +1,9 @@
+/* 
+            Camel Case
+    Si tenemos muchas palabras en una variable la primera letra de la palabra
+        es con minuscula y las siguientes palabras sus primera letra se colocaria
+        con mayusculas. ejemplo:
+        let nuevaVariable = 1;   let nuevaVariableNumeroUno = 1;
+    De esta forma trabaja javascript para mantener un orden.
+    
+*/
