3e68b7d76a1218a3a8e7995590376808e91f374e
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-03-31 10:54:48 -0500

    Se agrega proyecto Array

---
+++ 02-JAVASCRIPT/02-JAVASCRIPT desde cero - junior/10-Array/js/codigo.js
@@ -0,0 +1,16 @@
+/* 
+            Array
+    Es un contenedor de diferentes tipos de elementos que pueden ser de diferentes
+    tipos de datos.
+    Los arrays son objetos.
+
+
+*/
+
+//Crear array (todas las posiciones del array estan declaradas en caso de que muestre
+//  una posicion de array que no este definida te mostrara undefined)
+let frutas = ["Manzana","Banana","Pera"];
+
+//Mostrar array
+document.write(frutas[0]);
+
