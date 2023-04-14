a24e5a56291538b06b15525c503cf402d4139927
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-14 12:15:39 -0500

    Se agrega proyecto El DOM(teoria)

---
+++ 02-JAVASCRIPT/02-JAVASCRIPT desde cero - junior/22-El DOM(teoria)/js/codigo.js
@@ -0,0 +1,26 @@
+/* 
+            El DOM
+    - Definicion
+        El Document Object Model, es un interfaz que contiene todos los objetos estandares que nos permiten
+        representar un documento html,xml o xhtml, que a su vez tiene otra interfaz que nos permite
+        indicar que cosa combina con cada cosa y otra que nos permite trabajarla con lenguajes como JavaScript.
+    - Concepto Extendido.
+
+    - Nodo
+        Un nodo en el DOM es cualquier etiqueta del cuerpo, como un parrafo, el mismo body o incluso
+        las etiquetas de una lista.
+        - Document
+            El nodo document es el nodo raiz, a partir del cual derivan el resto de nodos.
+        - Element
+            Nodos definidos por etiqueta html
+        - Text
+            El texto dentro de un nodo elemento se considera un nuevo nodo hijo de tipo text (texto).
+        - Attribute
+            Los atributos de las etiquetas definen nodos, (en JavaScript no los veremos como nodos, sino
+                como informacion asociada al nodo de tipo element)
+        - Comentarios y otros
+            Los comentarios y otros elementos como las declaraciones doctype en cabecera de los documentos
+            HTML generan nodos.
+*/      
+
+
