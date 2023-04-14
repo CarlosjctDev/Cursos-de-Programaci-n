d26a16cd96dc64dff1b1ebc0f5b935982a56b23c
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-14 14:47:24 -0500

    Se agrega proyecto Atributo style

---
+++ 02-JAVASCRIPT/02-JAVASCRIPT desde cero - junior/27-Atributo style/js/codigo.js
@@ -0,0 +1,11 @@
+/* 
+        Atributos Style
+    
+    - Usos y ejemplos
+    - Propiedades Camel Case
+*/      
+
+const titulo = document.querySelector(".titulo");
+titulo.style.backgroundColor ="red";
+titulo.style.padding ="30px";
+titulo.style.border = "3px solid black"
