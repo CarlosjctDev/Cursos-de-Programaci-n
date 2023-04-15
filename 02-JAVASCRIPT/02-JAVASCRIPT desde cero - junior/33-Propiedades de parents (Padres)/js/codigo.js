b2870e37b215fa69daa769232ed1a4860ffc27b3
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-14 19:42:17 -0500

    Se agrega proyecto Propiedades de parents (Padres)

---
+++ 02-JAVASCRIPT/02-JAVASCRIPT desde cero - junior/33-Propiedades de parents (Padres)/js/codigo.js
@@ -0,0 +1,11 @@
+/* 
+        Propiedades de parents (Padres)
+    - parentElement
+        Selecciona el padre elemento de un elemento (solo html)
+    - parentNode
+        Elegir el padre nodo
+*/      
+const contenedor = document.querySelector(".contenedor");
+
+const h2_antiguo = document.querySelector(".h2");
+console.log(h2_antiguo.parentElement);
