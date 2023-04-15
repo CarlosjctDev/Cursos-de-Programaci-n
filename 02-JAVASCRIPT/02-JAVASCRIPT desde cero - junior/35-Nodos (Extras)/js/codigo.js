4128c3c805d5559a3102f034c2db10f834ebbd87
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-14 20:07:23 -0500

    Se agrega proyecto Nodos (Extras)

---
+++ 02-JAVASCRIPT/02-JAVASCRIPT desde cero - junior/35-Nodos (Extras)/js/codigo.js
@@ -0,0 +1,8 @@
+/* 
+        Nodos (Extras)
+    - closest()
+*/      
+
+const div = document.querySelector(".div-3");
+//Busca el contenedor que contenga a el elemento mas cercano de forma ascendente
+console.log(div.closest(".div"));
