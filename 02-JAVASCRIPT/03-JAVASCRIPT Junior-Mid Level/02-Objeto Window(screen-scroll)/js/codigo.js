c3ba9219b2c86df49ebb6c088dc489aa9a4bc20e
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-16 16:23:42 -0500

    Se agrega proyecto Objeto Window(screen-scroll)

---
+++ 02-JAVASCRIPT/03-JAVASCRIPT Junior-Mid Level/02-Objeto Window(screen-scroll)/js/codigo.js
@@ -0,0 +1,50 @@
+/* 
+        Objeto Window(screen-scroll)
+    - screen - devuelve una referencia al objeto de pantalla asociado con
+        la ventana.
+    - screenLeft - devuelve la distancia horizontal entre el borde izquierdo
+        del navegador y el borde izquierdo de la pantalla
+    - screenTop - devuelve la distancia vertical entre el borde superior del
+        navegador y el borde superior de pantalla
+    - scrollX - devuelve el numero de pixeles que el documento se desplaza 
+        actualmente horizontalmente.
+    - scrollY - devuelve el numero de pixeles que el documento se desplaza
+        actualmente verticalmente.
+    - scroll() - desplaza la ventana a un lugar particular en el
+        documento. (con options y con posiciones)
+
+    - resizeBy() - cambia el tamaño de la ventana actual en una
+        cantidad especifica.
+    - resizeTo() - redimensiona dinamicamente la ventana.
+    - moveBy() - Mueve la ventana en una ubicacion relativa.
+    - moveTo() - Mueve la ventana en una ubicacion absoluta.
+
+    - Objetos barprop
+                - locationbar
+                - menubar
+                - personalbar
+                - scrollbars
+                - statusbar
+                - toolbar
+    
+*/      
+
+// const screen = window.screen;
+// Se ven los objetos de screen en la consola
+// console.log(screen);
+// document.write(screen.availWidth);
+
+//Solo son propiedades de lectura(No se pueden modificar)
+// const screenLetf = window.screenLeft;
+// const screenTop = window.screenTop;
+// document.write(screenLetf);
+// document.write(screenTop);
+
+//Horizontal
+// const scrollX = window.scrollX;
+//Vertical
+// const scrollY = window.scrollY;
+
+//Colocar en consola (desplazar hacia abajo)
+// window.scroll(0,400);
+
