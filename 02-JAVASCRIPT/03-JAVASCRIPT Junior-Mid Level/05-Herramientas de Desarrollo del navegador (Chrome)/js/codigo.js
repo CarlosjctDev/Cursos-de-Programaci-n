cc8db34f717dbe8252caed1ee54cbf275d3069fc
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-18 19:48:57 -0500

    Se agrega proyecto Herramientas de Desarrollo del navegador (Chrome)

---
+++ 02-JAVASCRIPT/03-JAVASCRIPT Junior-Mid Level/05-Herramientas de Desarrollo del navegador (Chrome)/js/codigo.js
@@ -0,0 +1,19 @@
+/* 
+            Herramientas de Desarrollo del navegador (Chrome)
+    Pestaña Elements 
+        - Filtros y buscar por palabras claves
+        - Modificar, Crear y eliminar etiquetas
+        - Modificar propiedades de CSS
+        - Event Listeners
+        - Properties
+        - Opciones para copiar
+        - Seguir explorando
+    
+*/      
+
+/* Developer tools
+    Formas de abrir consola en google
+    Clic derecho al elemento y inspeccionar
+    F12
+    CTRL + MAYUSC + I
+*/
