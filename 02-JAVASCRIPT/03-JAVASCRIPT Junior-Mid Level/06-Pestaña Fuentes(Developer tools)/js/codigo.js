f66bfc46706526037922df6b3d29d855d3433628
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-18 20:49:52 -0500

    Se agrega proyecto Pestaña Fuentes(Developer tools)

---
+++ 02-JAVASCRIPT/03-JAVASCRIPT Junior-Mid Level/06-Pestaña Fuentes(Developer tools)/js/codigo.js
@@ -0,0 +1,15 @@
+/* 
+        Pestaña Fuentes(Developer tools)
+    - Ver y guardar archivos
+    - Informacion ofrecida
+    - Cambios en tiempo real
+    - Snippets - Ejecutar codigo por fragmentos
+*/      
+// ctrl + f  para buscar
+/*Sistemas de archivos en pestaña sources se puede
+importar los archivos y trabajar desde ahi y se 
+modifica en el editor*/
+
+/*En fragmentos se puede colocar codigo javascript
+y guardarlo para hacer pruebas y con ctrl intro 
+se ejecuta*/
