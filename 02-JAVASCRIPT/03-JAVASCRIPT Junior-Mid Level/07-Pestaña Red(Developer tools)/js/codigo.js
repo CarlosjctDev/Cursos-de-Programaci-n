9c5b7071701af086421391ee3c13c1eac2a49687
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-18 20:53:14 -0500

    Se agrega proyecto Pestaña Red(Developer tools)

---
+++ 02-JAVASCRIPT/03-JAVASCRIPT Junior-Mid Level/07-Pestaña Red(Developer tools)/js/codigo.js
@@ -0,0 +1,10 @@
+/* 
+        Pestaña Red(Developer tools)
+    - Interfaz
+    - Sort By y Filtros de Busqueda
+    - Limpiar cookies y cache
+    - Importar y Exportar
+*/      
+
+
+/*Contenido en el video, teoria*/
