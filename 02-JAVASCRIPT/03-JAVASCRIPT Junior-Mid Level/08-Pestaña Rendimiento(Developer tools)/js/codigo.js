992cf56fba9bbcf965a2762b9d6300395e5974b3
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-18 22:07:44 -0500

    Se agrega proyecto Pestaña Rendimiento(Developer tools)

---
+++ 02-JAVASCRIPT/03-JAVASCRIPT Junior-Mid Level/08-Pestaña Rendimiento(Developer tools)/js/codigo.js
@@ -0,0 +1,18 @@
+/* 
+        Pestaña Rendimiento(Developer tools)
+    - Uso y grabacion
+    - Screenshots de la grabacion
+    - Controls
+    - Overview
+        *FPS, CPU y Net
+    - Flame chart
+    - Details
+    - Sugerencias de grabacion
+        * Grabaciones cortas
+        * Evitar acciones innecesarias
+        * Inhabilita la cache del navegador
+        * Inhabilita las extensiones
+*/      
+// CTRL + SHIFT + P , mOSTRAR MONITOR DE RENDIMIENTO
+
+/*Contenido en el video, teoria*/
