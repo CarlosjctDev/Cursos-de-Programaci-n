6ba345d0f2458b3c40fe53e8a66d796e9aad9a42
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-04-20 15:02:20 -0500

    Se agrega proyecto Pestaña Aplicacion(Developer tools)

---
+++ 02-JAVASCRIPT/03-JAVASCRIPT Junior-Mid Level/09-Pestaña Aplicacion(Developer tools)/js/codigo.js
@@ -0,0 +1,21 @@
+/* 
+        Pestaña Aplicacion(Developer tools)
+    - Funcionamiento
+    - Apilamiento de mensajes
+    - Show timestamps
+    - Borrar el historial
+    - Conservar el historial
+    - Guardar el historial
+    - Eleccion del contexto de ejecucion
+    - Filtrado de los resultados de console
+    - Filtros por nivel de gravedad
+        * Todos (Muestra todos los resultados de console)
+        * Errors (Solo muestra los resultados de console erorr)
+        * warnings (Solo muestra los resultados de console warning)
+        * Info (Solo muestra los resultados de console info() )
+        * Logs (Solo muestra los resultados de console log())
+        * Debug (Solo muestra los resultados de console )
+*/      
+
+
+/*Contenido en el video, teoria*/
