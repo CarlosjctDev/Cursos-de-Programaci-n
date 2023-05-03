341b8ec4a609ca69a171a48f9a5255a94345dade
Carlosjct06Dev <carlosjulio0605@hotmail.com>
2023-05-03 09:44:27 -0500

    Se agrega proyecto Identificar codigo obsoleto

---
+++ 02-JAVASCRIPT/03-JAVASCRIPT Junior-Mid Level/19-Identificar codigo obsoleto/js/codigo.js
@@ -0,0 +1,26 @@
+/* 
+        Identificar codigo obsoleto
+    - ¿cuando se vuelve obsoleto algo?
+        - "Deprecated"
+        - Inutil
+        - No recomendado
+        - Con bugs o fallos
+        - Esta por ser reemplazado
+        - Hay mejores formas de hacerlo
+
+    - Desventajas
+        - Uso excesivo de recursos
+        - Codigo con bug o fallos
+        - codigo innecesariamente largo
+        - SEO (el codigo obsoleto afecta el seo)
+    
+    - ¿ Como verificar si esta obsoleto ?
+        - 1 de cada 3 webs utilizan librerias de javascript obsoletos
+        - verificar si tienen o usan funciones, metodos, objetos o metodologias obsoletas
+        - verificar en los sitios basados en estandares oficiales
+
+    - Detectar navegadores obsoletos
+        - Buscar en google "deteccion del navegador y cobertura a multiples navegadores"
+    
+*/ 
+
